5716fb4a6eb3302c0432718409039c1abfea623a
aditya2005uni <156787354+aditya2005uni@users.noreply.github.com>
2025-04-27 23:39:32 +0530

    Time: 751 ms (36.96%) | Memory: 9.1 MB (46.95%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -1,5 +1,6 @@
 class Solution {
-    void fun(vector<int>& cookies,vector<int>&v,int ind,int &res,int k){
+    public:
+    void fun(vector<int>& cookies, int k,vector<int>&v,int &res,int ind){
         if(ind>=cookies.size()){
             int maxi=*max_element(v.begin(),v.end());
             res=min(res,maxi);
@@ -8,7 +9,7 @@ class Solution {
         int c=cookies[ind];
         for(int i=0;i<k;i++){
             v[i]=v[i]+c;
-            fun(cookies,v,ind+1,res,k);
+            fun(cookies,k,v,res,ind+1);
             v[i]=v[i]-c;
         }
     }
@@ -16,7 +17,60 @@ public:
     int distributeCookies(vector<int>& cookies, int k) {
         vector<int>v(k,0);
         int res=INT_MAX;
-        fun(cookies,v,0,res,k);
+        fun(cookies,k,v,res,0);
         return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Solution {
+//     void fun(vector<int>& cookies,vector<int>&v,int ind,int &res,int k){
+//         if(ind>=cookies.size()){
+//             int maxi=*max_element(v.begin(),v.end());
+//             res=min(res,maxi);
+//             return;
+//         }
+//         int c=cookies[ind];
+//         for(int i=0;i<k;i++){
+//             v[i]=v[i]+c;
+//             fun(cookies,v,ind+1,res,k);
+//             v[i]=v[i]-c;
+//         }
+//     }
+// public:
+//     int distributeCookies(vector<int>& cookies, int k) {
+//         vector<int>v(k,0);
+//         int res=INT_MAX;
+//         fun(cookies,v,0,res,k);
+//         return res;
+//     }
+// };
