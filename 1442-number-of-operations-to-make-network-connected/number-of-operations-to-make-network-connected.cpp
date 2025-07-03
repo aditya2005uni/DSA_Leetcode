2399131c1c49240471928d0f4a7a6d0590821ddd
aditya2005uni <156787354+aditya2005uni@users.noreply.github.com>
2025-07-03 10:03:26 +0530

    Time: 24 ms (37.02%) | Memory: 46.7 MB (47.84%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,6 +1,6 @@
 class Solution {
     public:
-    int find(int i, vector<int>&par){
+    int find(int i,vector<int>&par){
         if(i==par[i]){
             return i;
         }
@@ -25,8 +25,8 @@ class Solution {
     }
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        vector<int>rank(n,0);
         vector<int>par(n);
+        vector<int>rank(n,0);
         for(int i=0;i<n;i++){
             par[i]=i;
         }
@@ -34,6 +34,7 @@ public:
             return -1;
         }
         int cnt=n;
+
         for(auto i:connections){
             int a=i[0];
             int b=i[1];
@@ -45,6 +46,22 @@ public:
         return cnt-1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -120,145 +137,5 @@ public:
 //             }    
 //         }
 //         return count-1;
-//     }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int find(int x, vector<int>& par) {
-//         if (x == par[x]) {
-//             return x;
-//         }
-//         return par[x] = find(par[x], par);
-//     }
-
-//     void uni(int x, int y, vector<int>& par, vector<int>& rank) {
-//         int px = find(x, par);
-//         int py = find(y, par);
-//         if (px == py) {
-//             return;
-//         }
-//         if (rank[px] > rank[py]) {
-//             par[py] = px;
-//         } 
-//         else if (rank[py] > rank[px]) {
-//             par[px] = py;
-//         } 
-//         else {
-//             par[px] = py;
-//             rank[py]++;
-//         }
-//     }
-
-//     int makeConnected(int n, vector<vector<int>>& connections) {
-//         if (connections.size() < n - 1) {
-//             return -1;
-//         }
-
-//         vector<int> par(n);
-//         vector<int> rank(n, 0);
-
-//         for (int i = 0; i < n; i++) {
-//             par[i] = i;
-//         }
-
-//         int comp = n; // Number of components
-//         for (auto& v : connections) {
-//             if (find(v[0], par) != find(v[1], par)) {
-//                 uni(v[0], v[1], par, rank);
-//                 comp--;
-//             }
-//         }
-
-//         return comp - 1;
-//     }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public:
-//     vector<int>par;
-//     vector<int>rank;
-//     int find(int x){
-//         if(x==par[x]){
-//             return x;
-//         }
-//         return par[x]=find(par[x]);
-//     }
-//     void uni(int x,int y){
-//         int px=find(x);
-//         int py=find(y);
-//         if(px==py){
-//             return;
-//         }
-//         if(rank[px]>rank[py]){
-//             par[py]=px;
-//         }
-//         else if(rank[py]>rank[px]){
-//             par[px]=py;
-//         }
-//         else{
-//             par[px]=py;
-//             rank[py]++;
-//         }
-//         return;
-//     }
-// public:
-//     int makeConnected(int n, vector<vector<int>>& connections) {
-//         if(connections.size()<n-1){
-//             return -1;
-//         }
-//         par.resize(n);
-//         rank.resize(n,0);
-//         for(int i=0;i<n;i++){
-//             par[i]=i;
-//         }
-//         int comp=n;//componets
-//         for(auto v:connections){
-//             if(find(v[0])!=find(v[1])){
-//                 uni(v[0],v[1]);
-//                 comp--;
-//             }
-//         }
-//         return comp-1;
 //     }
 // };
