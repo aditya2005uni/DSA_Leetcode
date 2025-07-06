c2ed265f39e185c8e481fb38206272d0b13a47b6
aditya2005uni <156787354+aditya2005uni@users.noreply.github.com>
2025-07-06 15:52:13 +0530

    Time: 3 ms (55.81%) | Memory: 27.5 MB (70.42%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,148 +1,29 @@
 class Solution {
-    private:
-    void dfs(vector<vector<int>>& grid,int i,int j,int m,int n,int &cal){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0){
+public:
+void dfs(vector<vector<int>>& grid,int i,int j,int n,int m,int &cnt){
+        if(i>=n || j>=m || i<0 || j<0 || grid[i][j]==0){
             return;
         }
-        cal++;
+        cnt++;
         grid[i][j]=0;
-        dfs(grid,i+1,j,m,n,cal);
-        dfs(grid,i-1,j,m,n,cal);
-        dfs(grid,i,j+1,m,n,cal);
-        dfs(grid,i,j-1,m,n,cal);
-    }
-public:
+        dfs(grid,i+1,j,n,m,cnt);
+        dfs(grid,i,j+1,n,m,cnt);
+        dfs(grid,i-1,j,n,m,cnt);
+        dfs(grid,i,j-1,n,m,cnt);
+    }   
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int area=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    int cal=0;
-                    dfs(grid,i,j,m,n,cal);
-                    area=max(area,cal);
+                    int cnt=0;
+                    dfs(grid,i,j,n,m,cnt);
+                    ans=max(ans,cnt);
                 }
             }
         }
-        return area;
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     private:
-//     void dfs(vector<vector<int>>&grid,int i,int j,int m,int n,int &c){
-//         if(i>=m || i<0 || j>=n || j<0 || grid[i][j]==0){
-//             return;
-//         }
-//         c++;
-//         grid[i][j]=0;
-//         dfs(grid,i+1,j,m,n,c);
-//         dfs(grid,i-1,j,m,n,c);
-//         dfs(grid,i,j+1,m,n,c);
-//         dfs(grid,i,j-1,m,n,c);
-//     }
-// public:
-//     int maxAreaOfIsland(vector<vector<int>>& grid) {
-//         int m=grid.size();
-//         int n=grid[0].size();
-//         int ans=0;
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(grid[i][j]==1){
-//                     int c=0;
-//                     dfs(grid,i,j,m,n,c);
-//                     ans=max(ans,c);
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class Solution {
-//     private:
-//     void dfs(int i,int j,vector<vector<int>>& grid,int &maxi,int m,int n){
-//         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0){
-//             return;
-//         }
-//         maxi++;
-//         grid[i][j]=0;
-//         dfs(i+1,j,grid,maxi,m,n);
-//         dfs(i-1,j,grid,maxi,m,n);
-//         dfs(i,j+1,grid,maxi,m,n);
-//         dfs(i,j-1,grid,maxi,m,n);
-//         return;
-//     }
-// public:
-//     int maxAreaOfIsland(vector<vector<int>>& grid) {
-//         int m=grid.size();
-//         int n=grid[0].size();
-//         int ans=0;
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(grid[i][j]==1){
-//                     int maxi=0;
-//                     dfs(i,j,grid,maxi,m,n);
-//                     ans=max(ans,maxi);
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
