eabf1e4f93b651ca69c25854ee1d39eb1881cc88
aditya2005uni <156787354+aditya2005uni@users.noreply.github.com>
2025-07-11 11:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (61.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,22 +1,17 @@
 class Solution {
+public:
     int fun(vector<vector<int>>& grid,int i,int j,int m,int n,vector<vector<int>>&dp){
-        if(i==m-1 && j==n-1){
-            return grid[i][j];
+        if(i<0 || j<0 || i>=m || j>=n){
+            return INT_MAX;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(i==m-1){
-            return dp[i][j]=grid[i][j]+fun(grid,i,j+1,m,n,dp);
+        if(i==m-1 && j==n-1){
+            return dp[i][j]=grid[i][j];
         }
-        else if(j==n-1){
-            return dp[i][j]=grid[i][j]+fun(grid,i+1,j,m,n,dp);
-        }
-        else{
-            return dp[i][j]=grid[i][j]+min(fun(grid,i+1,j,m,n,dp),fun(grid,i,j+1,m,n,dp));
-        }
+        return dp[i][j]=grid[i][j]+min(fun(grid,i+1,j,m,n,dp),fun(grid,i,j+1,m,n,dp));  
     }
-public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
