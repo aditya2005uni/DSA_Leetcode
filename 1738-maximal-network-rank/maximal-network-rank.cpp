d5090c2f331f6877ea8d705664a0adb19777689d
aditya2005uni <156787354+aditya2005uni@users.noreply.github.com>
2025-07-12 15:48:01 +0530

    Time: 11 ms (70.83%) | Memory: 37.8 MB (58.14%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<set<int>>adj(n);
+        vector<int>deg(n);
+        vector<vector<int>>adj(n,vector<int>(n,0));
         for(auto i:roads){
             int a=i[0];
             int b=i[1];
-            adj[a].insert(b);
-            adj[b].insert(a);
+            deg[a]++;
+            deg[b]++;
+            adj[a][b]=1;
+            adj[b][a]=1;
         }
         int cnt=0;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                int ans=adj[i].size()+adj[j].size();
-                if(adj[i].find(j)!=adj[i].end()){
+                int ans=deg[i]+deg[j];
+                if(adj[i][j]==1){
                     ans=ans-1;
                 }
                 cnt=max(cnt,ans);
