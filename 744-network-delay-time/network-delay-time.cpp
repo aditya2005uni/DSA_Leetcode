2c0beeec115d6e935f6c9eafb201bd0c3966ea04
aditya2005uni <156787354+aditya2005uni@users.noreply.github.com>
2025-07-14 10:55:38 +0530

    Time: 80 ms (74.58%) | Memory: 48 MB (28.53%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        vector<int>dist(n+1,INT_MAX);
         vector<vector<pair<int,int>>>adj(n+1);
         for(auto i:times){
             int u=i[0];
@@ -10,93 +8,32 @@ public:
             int w=i[2];
             adj[u].push_back({v,w});
         }
+        vector<int>dist(n+1,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    
         dist[k]=0;
         pq.push({0,k});
         while(!pq.empty()){
-            int cost_so_far=pq.top().first;
             int node=pq.top().second;
+            int cost_so_far=pq.top().first;
             pq.pop();
             for(auto v:adj[node]){
                 int u=v.first;
                 int cost=v.second;
-                if(cost+cost_so_far<dist[u]){
-                    dist[u]=cost+cost_so_far;
+                if(cost_so_far+cost<dist[u]){
+                    dist[u]=cost_so_far+cost;
                     pq.push({dist[u],u});
                 }
             }
         }
-        dist[0]=0;
-        int maxi=*max_element(dist.begin(),dist.end());
-        if(maxi==INT_MAX){
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans=max(ans,dist[i]);
+        }
+        if(ans==INT_MAX){
             return -1;
         }
-        return maxi;
+        return ans;
+
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-//         vector<vector<pair<int,int>>>adj(n+1);
-//         for(auto i:times){
-//             int u=i[0];
-//             int v=i[1];
-//             int w=i[2];
-//             adj[u].push_back({v,w});
-//         }
-//         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-//         vector<int>dist(n+1,INT_MAX);
-//         pq.push({0,k});
-//         dist[k]=0;
-//         while(!pq.empty()){
-//             int cost_sofar=pq.top().first;
-//             int node=pq.top().second;
-//             pq.pop();
-//             for(auto v:adj[node]){
-//                 int u=v.first;
-//                 int cost=v.second;
-//                 if(cost_sofar+cost<dist[u]){
-//                     dist[u]=cost_sofar+cost;
-//                     pq.push({dist[u],u});
-//                 }
-//             }
-//         }
-//         dist[0]=0;
-//         int maxi=*max_element(dist.begin(),dist.end());
-//         if(maxi==INT_MAX){
-//             return -1;
-//         }
-//         return maxi;
-//     }
-// };
